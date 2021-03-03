fc85bc937806e1294e8381f8d528c4403addb189
Joseph Broderick <penguinman98@usa.net>
2021-03-03 11:04:12 -0700

    Added block destruction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    // config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 paddleLaunchVector = new Vector2(2f, 10f);
+
+    // state
+    Vector2 paddleToBallVector;
+    bool ballLaunched = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!ballLaunched)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = paddleLaunchVector;
+            ballLaunched = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
     }
 }
